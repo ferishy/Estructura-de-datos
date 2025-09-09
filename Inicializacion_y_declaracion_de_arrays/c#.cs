305f7e7f4c35e69536a59c3f2b6b9e12747234fd
ferishy <fercastillo992003@gmail.com>
2025-09-09 12:03:43 -0700

    Crear e inicializar un array

--- Inicializacion_y_declaracion_de_arrays/c#.cs
+++ Inicializacion_y_declaracion_de_arrays/c#.cs
@@ -1,1 +1,1 @@
-int[] numeros = {1, 2, 3, 4, 5};
+int[] numeros = { 1, 2, 3, 4, 5 };
---
+++ Inicializacion_y_declaracion_de_arrays/java.java
@@ -0,0 +1,5 @@
+public class Ejemplo {
+    public static void main (string[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+    }
+}
